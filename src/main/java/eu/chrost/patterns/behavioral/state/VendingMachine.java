46bc7e7e453cb0b5fec45ca5a9cf5587de7c8f6d
Marcin Chrost <mchrost82@gmail.com>
2023-12-11 15:05:44 +0100

    State pattern - solution

--- src/main/java/eu/chrost/patterns/behavioral/state/VendingMachine.java
+++ src/main/java/eu/chrost/patterns/behavioral/state/VendingMachine.java
@@ -2,6 +2,7 @@ package eu.chrost.patterns.behavioral.state;
 
 import lombok.RequiredArgsConstructor;
 
+import static eu.chrost.patterns.behavioral.state.VendingMachine.State.IDLE;
 import static eu.chrost.patterns.behavioral.state.VendingMachine.State.PAID;
 import static eu.chrost.patterns.behavioral.state.VendingMachine.State.PAYING;
 import static eu.chrost.patterns.behavioral.state.VendingMachine.State.SOLD_OUT;
@@ -41,38 +42,57 @@ class VendingMachine {
     }
 
     public String dispenseProduct() {
-        if (productsAmount <= 0) {
-            return "Items sold out";
-        }
-        if (insertedMoneyAmount >= productPrice) {
-            var change = insertedMoneyAmount - productPrice;
-            insertedMoneyAmount = 0;
-            productsAmount--;
-            return String.format("Dispensing product and change %d", change);
-        } else {
-            var toPay = productPrice - insertedMoneyAmount;
-            return String.format("Cannot dispense product - amount %d must be paid", toPay);
+        switch (state) {
+            case IDLE:
+            case PAYING:
+                var toPay = productPrice - insertedMoneyAmount;
+                return String.format("Cannot dispense product - amount %d must be paid", toPay);
+            case PAID:
+                var change = insertedMoneyAmount - productPrice;
+                insertedMoneyAmount = 0;
+                productsAmount--;
+                if (productsAmount > 0) {
+                    state = IDLE;
+                } else {
+                    state = SOLD_OUT;
+                }
+                return String.format("Dispensing product and change %d", change);
+            case SOLD_OUT:
+                return "Items sold out";
+            default:
+                throw new IllegalStateException("Unknown machine state");
         }
     }
 
     public String cancelPurchase() {
-        if (insertedMoneyAmount > 0) {
-            var toReturn = insertedMoneyAmount;
-            insertedMoneyAmount = 0;
-            return String.format("Returning %d", toReturn);
-        } else {
-            return "Nothing happens";
+        switch (state) {
+            case IDLE:
+            case SOLD_OUT:
+                return "Nothing happens";
+            case PAYING:
+            case PAID:
+                var toReturn = insertedMoneyAmount;
+                insertedMoneyAmount = 0;
+                state = IDLE;
+                return String.format("Returning %d", toReturn);
+            default:
+                throw new IllegalStateException("Unknown machine state");
         }
     }
 
     public String refill(int productsBatchAmount) {
-        if (insertedMoneyAmount > 0) {
-            return "Cannot refill - purchase in progress";
-        } else if (productsAmount > 0) {
-            return "Cannot refill - there are still some products";
-        } else {
-            productsAmount = productsBatchAmount;
-            return "Machine refilled";
+        switch (state) {
+            case IDLE:
+                return "Cannot refill - there are still some products";
+            case PAYING:
+            case PAID:
+                return "Cannot refill - purchase in progress";
+            case SOLD_OUT:
+                productsAmount = productsBatchAmount;
+                state = IDLE;
+                return "Machine refilled";
+            default:
+                throw new IllegalStateException("Unknown machine state");
         }
     }
 

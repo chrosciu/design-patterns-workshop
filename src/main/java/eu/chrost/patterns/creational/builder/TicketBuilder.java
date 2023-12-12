34ccfc14fdc85fc90da352b11bc9f98c15d9929d
Marcin Chrost <mchrost82@gmail.com>
2023-12-12 08:47:14 +0100

    Builder pattern - solution

--- src/main/java/eu/chrost/patterns/creational/builder/TicketBuilder.java
+++ src/main/java/eu/chrost/patterns/creational/builder/TicketBuilder.java
@@ -1,5 +1,9 @@
 package eu.chrost.patterns.creational.builder;
 
+import lombok.NonNull;
+
+import static eu.chrost.patterns.creational.builder.TicketBuilder.PriceType.REGULAR;
+
 class TicketBuilder {
     enum TicketType {
         AIR,
@@ -13,19 +17,54 @@ class TicketBuilder {
         FLEX
     }
 
+    private TicketType ticketType = null;
+    private PriceType priceType = REGULAR;
+
     static TicketBuilder create() {
-        return null;
+        return new TicketBuilder();
     }
 
-    TicketBuilder ticketType(TicketType ticketType) {
-        return null;
+    TicketBuilder ticketType(@NonNull TicketType ticketType) {
+        this.ticketType = ticketType;
+        return this;
     }
 
-    TicketBuilder priceType(PriceType priceType) {
-        return null;
+    TicketBuilder priceType(@NonNull PriceType priceType) {
+        this.priceType = priceType;
+        return this;
     }
 
     Ticket build() {
-        return null;
+        if (null == ticketType) {
+            throw new IllegalStateException("Ticket type is not set");
+        }
+        var price = createPrice();
+        return createTicket(price);
+    }
+
+    private Price createPrice() {
+        switch (priceType) {
+            case REGULAR:
+                return new RegularPrice();
+            case DISCOUNT:
+                return new DiscountPrice();
+            case FLEX:
+                return new FlexPrice();
+            default:
+                throw new IllegalStateException("Unknown price type");
+        }
+    }
+
+    private Ticket createTicket(Price price) {
+        switch (ticketType) {
+            case AIR:
+                return new AirTicket(price);
+            case RAILWAY:
+                return new RailwayTicket(price);
+            case FERRY:
+                return new FerryTicket(price);
+            default:
+                throw new IllegalStateException("Unknown ticket type");
+        }
     }
 }

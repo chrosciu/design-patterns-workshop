4ce46e60d1f260065afa7328b3d3aa8978a6ecb4
Marcin Chrost <mchrost82@gmail.com>
2023-12-12 10:31:52 +0100

    Observer pattern - partial solution

--- src/main/java/eu/chrost/patterns/behavioral/observer/Checkbox.java
+++ src/main/java/eu/chrost/patterns/behavioral/observer/Checkbox.java
@@ -3,16 +3,12 @@ package eu.chrost.patterns.behavioral.observer;
 import lombok.Getter;
 import lombok.Setter;
 
-class Checkbox {
-    @Setter
-    private Input input;
-    @Setter
-    private Button button;
+class Checkbox extends AbstractPublisher {
     @Getter
     private boolean checked = false;
 
     public void setChecked(boolean checked) {
         this.checked = checked;
-        button.setEnabled(!checked || !input.getText().isBlank());
+        notifyAllSubscribers(new CheckboxChangeEvent(this));
     }
 }

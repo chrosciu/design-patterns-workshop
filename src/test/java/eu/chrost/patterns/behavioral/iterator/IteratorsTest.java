1dc90f634e18e091b90c8a976c535e14bcdf3c61
Marcin Chrost <mchrost82@gmail.com>
2024-07-10 10:07:55 +0200

    Iterator test for empty list

--- src/test/java/eu/chrost/patterns/behavioral/iterator/IteratorsTest.java
+++ src/test/java/eu/chrost/patterns/behavioral/iterator/IteratorsTest.java
@@ -4,11 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Spliterators;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class IteratorsTest {
     private final List<Integer> list = List.of(2, 3, 1);
@@ -23,6 +25,13 @@ public class IteratorsTest {
     void reverseIteratorTest() {
         Iterator<Integer> iterator = Iterators.reverseIterator(list);
         assertThat(toList(iterator)).containsExactly(1, 3, 2);
+    }
+
+    @Test
+    void emptyListTest() {
+        Iterator<Integer> iterator = Iterators.reverseIterator(List.of());
+        assertThat(iterator.hasNext()).isFalse();
+        assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> iterator.next());
     }
 
     private static <T> List<T> toList(Iterator<? extends T> iterator) {

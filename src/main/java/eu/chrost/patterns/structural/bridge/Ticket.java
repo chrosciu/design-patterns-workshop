93baa28a96c3826ff5fb47a0fa8cc2a7c179bd9e
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 12:20:51 +0200

    Bridge pattern - partial solution

--- src/main/java/eu/chrost/patterns/structural/bridge/Ticket.java
+++ src/main/java/eu/chrost/patterns/structural/bridge/Ticket.java
@@ -1,7 +1,14 @@
 package eu.chrost.patterns.structural.bridge;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 abstract class Ticket {
+    private final Price price;
+
     abstract String getTransportationType();
 
-    abstract String getPriceType();
+    String getPriceType() {
+        return price.getPriceType();
+    }
 }

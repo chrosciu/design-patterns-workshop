a4301423d6fc2f936549b5313dcee186605f8eca
Marcin Chrost <mchrost82@gmail.com>
2024-07-11 16:15:52 +0200

    Chain of responsibility pattern - introduced RegularDeveloper

--- src/main/java/eu/chrost/patterns/behavioral/chain/SupportCenter.java
+++ src/main/java/eu/chrost/patterns/behavioral/chain/SupportCenter.java
@@ -2,11 +2,14 @@ package eu.chrost.patterns.behavioral.chain;
 
 class SupportCenter {
     private final JuniorDeveloper juniorDeveloper = new JuniorDeveloper();
+    private final RegularDeveloper regularDeveloper = new RegularDeveloper();
     private final SeniorDeveloper seniorDeveloper = new SeniorDeveloper();
 
     String solve(Bug bug) {
-        if (bug.getSeverity().getRank() <= Severity.NORMAL.getRank()) {
+        if (bug.getSeverity().getRank() <= Severity.MINOR.getRank()) {
             return juniorDeveloper.solve(bug);
+        } else if (bug.getSeverity().getRank() <= Severity.MAJOR.getRank()) {
+            return regularDeveloper.solve(bug);
         } else {
             return seniorDeveloper.solve(bug);
         }
